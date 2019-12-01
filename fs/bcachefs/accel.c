6ee66d2e864ec8610b20145ec6e8a489700f9e31
Robbie Litchfield <blam.kiwi@gmail.com>
2019-12-01 15:51:18 +1300

    Added different benchmark cases. Fixed CRC64 copmatibility with ISAL

--- fs/bcachefs/accel.c
+++ fs/bcachefs/accel.c
@@ -16,10 +16,14 @@ static u64 kernel_crc64(u64 crc, const void* p, size_t len) {
 	return crc64_be(crc, p, len);
 }
 
-static u64 isal_crc64(u64 crc, const void* p, size_t len) {
+static u64 isal_crc64(u64 crc, const void* p, size_t len) { 
+	u64 state = ~crc;
+
 	kernel_fpu_begin();
-	return crc64_ecma_norm(crc, (const unsigned char*)p, len);
+	state =  crc64_ecma_norm(state, (const unsigned char*)p, len);
 	kernel_fpu_end();
+
+	return ~state;
 }
 
 static u32 kernel_crc32c(u32 crc, const void* p, size_t len) {
@@ -33,6 +37,8 @@ static u32 isal_crc32c(u32 crc, const void* p, size_t len) {
 }
 
 u64 accel_crc64(u64 crc, const void* p, size_t len) {
+	return kernel_crc64(crc, p, len);
+
 	#ifdef CONFIG_BCACHEFS_ISAL_BACKEND
 	return isal_crc64(crc, p, len);
 	#else
@@ -41,6 +47,8 @@ u64 accel_crc64(u64 crc, const void* p, size_t len) {
 }
 
 u64 accel_crc32c(u32 crc, const void* p, size_t len) {
+	return kernel_crc32c(crc, p, len);
+
 	#ifdef CONFIG_BCACHEFS_ISAL_BACKEND
 	return isal_crc32c(crc, p, len);
 	#else
@@ -48,78 +56,73 @@ u64 accel_crc32c(u32 crc, const void* p, size_t len) {
 	#endif
 }
 
-static const int CRC_BENCH_SIZE = 1024 * 1024 * 1024; // 1MB
+static const int MB = 1024 * 1024;
+static const int LARGE_BLOCK = 2 * MB; // Filesystem large IOs (Media etc)
+static const int SMALL_BLOCK = 4096; // Filesystem small IOs (Databases etc)
+static const int CACHE_THRASH = 512 * MB; // Larger than Epyc ROME L3 Cache
 static const int WARMUP_ITER = 3;
-static const int BENCH_ITER = 11;
-static const int NS_IN_S = 1000000000;
+static const int BENCH_ITER = 5;
 
-static void bench_crc32c(u32(*f)(u32, const void*, size_t), const char* name) {
-	char* buf = vmalloc(CRC_BENCH_SIZE);
+static void bench_crc32c(u32(*f)(u32, const void*, size_t), size_t bench_size, const char* name) {
+	char* buf = vmalloc(bench_size);
 	int i;
-	u64 quotient;
-	u64 remainder;
 	u64 sum = 0;
-	
-	for(i = 0; i < CRC_BENCH_SIZE; i++) {
+
+	for(i = 0; i < bench_size; i++) {
 		buf[i] = (char)i;
 	}
 
 	for(i = 0; i < WARMUP_ITER; i++) {
-		f(~0, buf, CRC_BENCH_SIZE);
+		f(~0, buf, bench_size);
 	}
 
 	for(i = 0; i < BENCH_ITER; i++) {
 		u64 begin;
 		u64 end;
+		u64 diff;
 
 		begin = ktime_get_ns();
-		f(~0, buf, CRC_BENCH_SIZE);
+		f(~0, buf, bench_size);
 		end = ktime_get_ns();
 
-		sum += end - begin;
+		diff = end - begin;
+		sum += diff;
 	}
 
-	sum /= 11;
-	quotient = sum / NS_IN_S;
-	remainder = sum % NS_IN_S;
-
-	printk("%s: %llu.%llu MiB/s. Average Time: %llu", name, quotient, remainder, sum);
+	sum /= BENCH_ITER;
+	printk("%s: %llu ns\n", name, sum);
 
 	vfree(buf);
 }
 
-static void bench_crc64(u64(*f)(u64, const void*, size_t), const char* name) {
-	char* buf = vmalloc(CRC_BENCH_SIZE);
+static void bench_crc64(u64(*f)(u64, const void*, size_t), size_t bench_size, const char* name) {
+	char* buf = vmalloc(bench_size);
 	int i;
-	u64 quotient;
-	u64 remainder;
 	u64 sum = 0;
-	
-	for(i = 0; i < CRC_BENCH_SIZE; i++) {
+
+	for(i = 0; i < bench_size; i++) {
 		buf[i] = (char)i;
 	}
 
 	for(i = 0; i < WARMUP_ITER; i++) {
-		f(~0, buf, CRC_BENCH_SIZE);
+		f(~0, buf, bench_size);
 	}
 
 	for(i = 0; i < BENCH_ITER; i++) {
 		u64 begin;
 		u64 end;
+		u64 diff;
 
 		begin = ktime_get_ns();
-		f(~0, buf, CRC_BENCH_SIZE);
+		f(~0, buf, bench_size);
 		end = ktime_get_ns();
 
-		sum += end - begin;
+		diff = end - begin;
+		sum += diff;
 	}
 
-	sum /= 11;
-	sum *= sum * NS_IN_S;
-	quotient = sum / NS_IN_S;
-	remainder = sum % NS_IN_S;
-
-	printk("%s: %llu.%llu MiB/s. Average Time: %llu", name, quotient, remainder, sum);
+	sum /= BENCH_ITER;
+	printk("%s: %llu ns\n", name, sum);
 
 	vfree(buf);
 }
@@ -143,16 +146,27 @@ int accel_benchmark(const char* prim) {
 	}
 
 	if(crc32c) {
-		bench_crc32c(&kernel_crc32c, "kernel crc32c");
+		bench_crc32c(&kernel_crc32c, CACHE_THRASH, "KERNEL CRC32C 512MB");
+		bench_crc32c(&kernel_crc32c, LARGE_BLOCK, "KERNEL CRC32C 2MB");
+		bench_crc32c(&kernel_crc32c, SMALL_BLOCK, "KERNEL CRC32C 4KB");
+
 		#ifdef CONFIG_BCACHEFS_ISAL_BACKEND
-		bench_crc32c(&isal_crc32c, "isal crc32c");
+		bench_crc32c(&isal_crc32c, CACHE_THRASH, "ISAL CRC32C 512MB");
+		bench_crc32c(&isal_crc32c, LARGE_BLOCK, "ISAL CRC32C 2MB");
+		bench_crc32c(&isal_crc32c, SMALL_BLOCK, "ISAL CRC32C 4KB");
 		#endif
 	}
 
 	if(crc64) {
-		bench_crc64(&kernel_crc64, "kernel crc64");
+		bench_crc64(&kernel_crc64, CACHE_THRASH, "KERNEL CRC64 512MB");
+		bench_crc64(&kernel_crc64, LARGE_BLOCK, "KERNEL CRC64 2MB");
+		bench_crc64(&kernel_crc64, SMALL_BLOCK, "KERNEL CRC64 4KB");
+
 		#ifdef CONFIG_BCACHEFS_ISAL_BACKEND
-		bench_crc64(&isal_crc64, "isal crc64");
+		bench_crc64(&isal_crc64, CACHE_THRASH, "ISAL CRC64 512MB");
+		bench_crc64(&isal_crc64, LARGE_BLOCK, "ISAL CRC64 2MB");
+		bench_crc64(&isal_crc64, SMALL_BLOCK, "ISAL CRC64 4KB");
+		
 		#endif
 	}
 
